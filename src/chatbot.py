72ba55f91a208334babc508efc92984b032d57fa
Omer F. Yuce <68684512+f4ruq@users.noreply.github.com>
2025-08-06 01:16:46 +0300

    Update chatbot.py

--- src/chatbot.py
+++ src/chatbot.py
@@ -1,114 +1,65 @@
 import torch
-from transformers import AutoModelForSequenceClassification, AutoTokenizer, pipeline
-from deep_translator import GoogleTranslator
-from questions import hypertension, flu, heart_attack, stroke, pneumonia, arrhythmia, gastritis, diabeties, cancer, anaphylaxis
+from transformers import AutoModelForSequenceClassification, AutoTokenizer, TrainingArguments, Trainer
+from datasets import load_dataset
 
-translator = GoogleTranslator(source="en", target="tr")
+dataset = load_dataset("csv", data_files="bert_ready.csv", split="train")
 
-model_path = "./tunedbert"
-tokenizer = AutoTokenizer.from_pretrained(model_path)
-model = AutoModelForSequenceClassification.from_pretrained(model_path)
+# %80 training, %20 test
+train_test_split = dataset.train_test_split(test_size=0.2)
+train_dataset = train_test_split["train"]
+eval_dataset = train_test_split["test"]
 
-label_mapping = {
-    0: "Hypertension", 1: "Flu", 2: "Heart Attack", 3: "Stroke",
-    4: "Pneumonia", 5: "Arrhythmia", 6: "Gastritis",
-    7: "Diabetes", 8: "Cancer", 9: "Anaphylaxis"
-}
+model_name = "google-bert/bert-base-uncased"
+tokenizer = AutoTokenizer.from_pretrained(model_name)
 
-pipe = pipeline("text-classification", model=model, tokenizer=tokenizer, device=-1, top_k=None)
+# label encoding
+label_classes = sorted(set(dataset["label"])) 
+label_mapping = {label: idx for idx, label in enumerate(label_classes)}
 
-print("\nhastalık teşhisi için bir semptom metni giriniz.")
-print("çıkmak için 'exit' yazın.\n")
+model = AutoModelForSequenceClassification.from_pretrained(model_name, num_labels=len(label_classes))
 
-while True:
-    user_input = input("metin giriniz: ")
-    if user_input.lower() == "exit":
-        print("çıkılıyor...")
-        break
+# tokenization
+def tokenize_function(example):
+    tokens = tokenizer(example["text"], padding="max_length", truncation=True, max_length=128)
+    tokens["labels"] = label_mapping[example["label"]]  
+    return tokens
 
-    output = pipe(user_input)  
+# tokenized dataset
+tokenized_train = train_dataset.map(tokenize_function, batched=False, remove_columns=train_dataset.column_names)
+tokenized_eval = eval_dataset.map(tokenize_function, batched=False, remove_columns=eval_dataset.column_names)
 
-    best_prediction = output[0][0]  
-    best_label_index = int(best_prediction['label'].replace("LABEL_", ""))
-    best_label = label_mapping[best_label_index]
-    best_score = best_prediction['score']
+# model parameters
+training_args = TrainingArguments(
+    output_dir="./bert-medical",
+    evaluation_strategy="epoch",    
+    save_strategy="epoch",
+    per_device_train_batch_size=8,  
+    per_device_eval_batch_size=8,
+    gradient_accumulation_steps=1,  
+    num_train_epochs=15, 
+    save_total_limit=2,
+    logging_dir="./logs",
+    logging_steps=10,
+    remove_unused_columns=False,
+    report_to="none"
+)
 
-    print(best_label)
+# training
+trainer = Trainer(
+    model=model,
+    args=training_args,
+    train_dataset=tokenized_train,
+    eval_dataset=tokenized_eval
+)
 
-    print("\nTahminler:")
-    for result in output[0]:  
-        label_index = int(result['label'].replace("LABEL_", ""))
-        predicted_label = label_mapping[label_index]
-        score = result['score']
-        print(f"{predicted_label}: {score:.2f}")
-    
-    #print(best_label)
-    
-    symptom_score = 0
-    
-    if(best_label=="Flu"):
-        for question in flu:
-            answer = input(question)
-            if(answer == "yes"):
-                symptom_score += 1
-    
-    elif(best_label=="Hypertension"):
-        for question in hypertension:
-            answer = input(question)
-            if(answer == "yes"):
-                symptom_score += 1
-    
-    elif(best_label=="Heart Attack"):
-        for question in heart_attack:
-            answer = input(question)
-            if(answer.lower() == "yes"):
-                symptom_score +=1
-    
-    elif(best_label=="Stroke"):
-        for question in stroke:
-            answer = input(question)
-            if(answer.lower()=="yes"):
-                symptom_score +=1
-    
-    elif(best_label=="Pneumonia"):
-        for question in pneumonia:
-            answer = input(question)
-            if(answer=="yes"):
-                symptom_score +=1
-    
-    elif(best_label=="Arrythmia"):
-        for question in arrhythmia:
-            answer = input(question)
-            if(answer=="yes"):
-                symptom_score +=1
-    
-    elif(best_label=="gastritis"):
-        for question in gastritis:
-            answer = input(question)
-            if(answer=="yes"):
-                symptom_score +=1
-    
-    elif(best_label=="diabeties"):
-        for question in diabeties:
-            answer = input(question)
-            if(answer=="yes"):
-                symptom_score +=1
-    
-    elif(best_label=="cancer"):
-        for question in cancer:
-            answer = input(question)
-            if(answer=="yes"):
-                symptom_score +=1
+try:
+    trainer.train()
+except KeyboardInterrupt:
+    print("Eğitim manuel olarak durduruldu. Kaydediliyor...")
+    trainer.save_model("./bert-medical/manual-stop")
+    tokenizer.save_pretrained("./bert-medical/manual-stop")
 
-    elif(best_label=="anaphylaxis"):
-        for question in anaphylaxis:
-            answer = input(question)
-            if(answer=="yes"):
-                symptom_score +=1
+model.save_pretrained("./tunedbert_tr")
+tokenizer.save_pretrained("./tunedbert_tr")
 
-    """if(symptom_score < 3):"""
-        
-    print("symptom score: ",symptom_score)
-
-print("\n")
-#print(label_mapping[0])
+print("Model eğitimi tamamlandı ve kaydedildi.")
